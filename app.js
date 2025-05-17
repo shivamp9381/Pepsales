a1ae6a9e5ba28701b0bf0b1e0115670622a5e0e6
shivamp9381 <shivamp9381@gmail.com>
2025-05-17 15:38:04 +0530

    Some deployment errors fixed

--- app.js
+++ app.js
@@ -56,10 +56,8 @@ const app = express();
 app.use(express.json());
 app.use('/api', routes);
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('Connected to MongoDB'))
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
 app.listen(3000, () => console.log('Server running on port 3000'));
